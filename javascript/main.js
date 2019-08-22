787771e3e48378249f5fd96ad679d892d6cf751f
annaelizabeth2019 <annapeterson89@gmail.com>
2019-08-22 18:26:14 -0500

    adds rps, etc

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
 // Write a toggle function for a child of the selected element!!!
+function toggleButton(event) {
+  const text = event.target.parentNode.children[1];
+  console.log(event.target.parentNode.children[1].style.display);
+
+  if (text.style.display === "none") {
+    console.log("true");
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
