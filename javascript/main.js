9fd0833c10e80486b65c0d6ee825368109116182
annaelizabeth2019 <annapeterson89@gmail.com>
2019-09-06 18:54:37 -0500

    preparing for lesson 1

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,26 @@
 // Write a toggle function for a child of the selected element!!!
+const scope = "Higher scope";
 function toggleButton(event) {
   const text = event.target.parentNode.children[1];
-  console.log(event.target.parentNode.children[1].style.display);
 
   if (text.style.display === "none") {
-    console.log("true");
     text.style.display = "block";
   } else {
     text.style.display = "none";
   }
 }
+
+button = document.getElementById("myEvent");
+button.onclick = event => {
+  console.log("onclick");
+};
+button.onclick = event => {
+  console.log("onclick 2");
+};
+
+button.addEventListener("click", event => {
+  console.log("addEventListener", event);
+});
+button.addEventListener("click", event => {
+  console.log("addEventListener 2");
+});
